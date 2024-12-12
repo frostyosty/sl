4e1617f80b3906f1e1be3875628e4c1d79b5c172
frostyosty <92064099+frostyosty@users.noreply.github.com>
2024-12-13 07:48:48 +1300

    Delete test/esbuild.config.js

--- test/esbuild.config.js
+++
@@ -1,8 +0,0 @@
-require('esbuild').build({
-    entryPoints: ['index.js'],
-    bundle: true,
-    outfile: 'bundle.js',
-    platform: 'browser',
-    format: 'esm',
-    minify: true,
-}).catch(() => process.exit(1));
